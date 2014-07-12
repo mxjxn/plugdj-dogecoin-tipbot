caec7751f5f3474ee7b03a936b5c2f6fecad7929
Max Jackson <modulations@gmail.com>
2014-07-12 19:48:12 -0400

    formatted README.md

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
-DOGECOIN TIP BOT for PLUG.DJ
+#DOGECOIN TIP BOT 
+##for PLUG.DJ
 
 uses npm packages node-dogecoin, redis, plugapi and nconf.
 
 you must have dogecoin running in the command line. I have only done this for linux. I installed dogecoin under another user for security purposes.
+
 you must also have redis running. bot.js uses its default credentials. redis is used to keep tabs on which user has which id and which id belongs to which user.
 
-command delimiter is . (period)
+by default, command delimiter is `.` (period)
 
-commands are 
+###commands are...
 
-deposit // returns a wallet address you can deposit to
+`deposit` returns a wallet address you can deposit to
 
-withdraw [your-dogecoin-address] // sends your entire balance within the bot to your-dogecoin-address
+`withdraw [your-dogecoin-address]` sends your entire balance within the bot to your-dogecoin-address
 
-tip [@username] [value] // tips user the intended value (assuming you have it in that wallet)
+`tip [@username] [value]` tips user the intended value (assuming you have it in that wallet)
 
-balance // displays your current balance in chat 
+`balance` displays your current balance in chat 
--- bot.js
+++ bot.js
@@ -1,55 +1,82 @@
 (function() {
+/**
+ *
+ * DOGECOIN TIP BOT for Plug.dj. 
+ * written by Max Jackson ( moncrey )
+ * relies on Redis and dogecoin running in the background.
+ *
+ **/
 
-var PlugAPI = require('plugapi'),
 
-    nconf = require('nconf');
+// Load config files
+var nconf = require('nconf'),
+misc = require('./commands');
 
-    nconf.file("config.json")
-        .file("secret_config.json")
-        .defaults({
-            "doge":{"user":false,"pass":false},
-            "plug":{"auth":false,"room":false}
-        });
+nconf.file("config.json")
+    .file("secret_config.json")
+    .defaults({
+        "doge":{"user":false,"pass":false},
+        "plug":{"auth":false,"room":false}
+    });
 
+// Set important variables from config files.
 var AUTH = nconf.get('plug:auth'),
-    ROOM = nconf.get('plug:room');
-    DOGE_USER = nconf.get("doge:user"),
-    DOGE_PASS = nconf.get("doge:pass"),
-    bot = new PlugAPI(AUTH),
-    insult = nconf.get('insult'),
-    tx_fee = nconf.get('tx_fee'),
-    currentDJ="";
+ROOM = nconf.get('plug:room'),
+DOGE_USER = nconf.get("doge:user"),
+DOGE_PASS = nconf.get("doge:pass"),
+insult = nconf.get('insult'),
+tx_fee = nconf.get('tx_fee'),
+
+PlugAPI = require('plugapi'),
+bot = new PlugAPI(AUTH),
+
+currentDJ="";
 
 if(!AUTH || !ROOM){
-    console.log('missing config');
+    console.log('missing config, cannot load bot');
     return false;
 }
 
-/* REDIS STUFF */
+/* REDIS STUFF, used to keep track of username <--> id relationships */
 var redis = require('redis'),
-    rClient = rClient || redis.createClient(),
+rClient = rClient || redis.createClient(),
 
-/* REPL */
-    repl = require('repl'),
-    botrepl = repl.start("bot>"),
+/* node REPL for behind the scenes tweaks */
+repl = require('repl'),
+botrepl = repl.start("bot>"),
+
 /* DOGECOIN */
-    dogecoin = require('node-dogecoin')();
+dogecoin = require('node-dogecoin')();
 
 // INITIALIZE BOT,DOGE,REDIS
 
 bot.connect(ROOM);
 dogecoin.auth(DOGE_USER, DOGE_PASS);
-rClient.on("error", function (err) { console.log("REDIS Error: " + err); }); 
+
+// make repl references to important variables
 botrepl.context.dogecoin=dogecoin;
 botrepl.context.bot = bot;
 botrepl.context.redis = rClient;
 
-//for fun
+rClient.on("error", function (err) { console.log("REDIS Error: " + err); }); 
 
-bot.on("roomJoin", function(room){ 
-    refreshRedis();
+
+/*
+ * PLUG.DJ EVENT RESPONDERS
+ */
+
+/* event roomChanged -- when the bot enters a room */
+bot.on("roomChanged", function() {
+	console.log("[+] Joined " + ROOM);
 });
 
+/* event roomJoin -- when the bot is fully connected */
+bot.on("roomJoin", function(room){ 
+    refreshRedis();
+    console.log('room joined.');
+});
+
+/* event userJoin -- someone else has entered the current room */
 bot.on("userJoin", function(data){
     rClient.select(0);
     rClient.set(data.username,data.id);
@@ -57,6 +84,7 @@ bot.on("userJoin", function(data){
     rClient.set(data.id,data.username);
 });
 
+/* event userLeave -- a user has left the current room */
 bot.on("userLeave", function(data){
     var byeName;
     rClient.select(1);
@@ -66,12 +94,13 @@ bot.on("userLeave", function(data){
     rClient.expire(byeName,2);
 });
 
-// only used for REPL console trace.
+/* event chat -- event fires whenever someone chats in the room */
 bot.on("chat", function(data){ console.log(data.from + ": " + data.message) });
 
+/* event djAdvance -- fires between songs, when a user joins an empty queue or when a user leaves the deck, leaving the queue empty */
 bot.on("djAdvance", function(data){
-
     var usrname;
+    //conditional because of varying json formatting.
     if(typeof data['dj'] !== 'undefined'){
     	usrname = data.dj.user.username;
     }
@@ -86,148 +115,97 @@ bot.on("djAdvance", function(data){
     bot.sendChat("/me " + usrname +" playing " + data.media.title + " by " + data.media.author);
 });
 
-
-bot.on("roomChanged", function() {
-	console.log("[+] Joined " + ROOM);
-
-
-
-});
-
+/* event command -- triggered when the command delimiter is the first character in a new chat line. '.' (period) by default */
 bot.on("command", function(data){
-    //console.log(data);
-    
     switch(data.command){
-    case "clap":bot.sendChat(":clap: :clap: :clap: :clap: BRAVO!");break;
-    case "moo":bot.sendChat(":cow: m0000000 :cow2:");break;
-    case "roll":
-        var num=0;
-        if( data.args[0] == "" || data.args[0] == "d6")
-            num = Math.floor(Math.random() * 6) + 1;
-        else if(data.args[0] == "d20")
-            num = Math.floor(Math.random() * 20) + 1;
-        else{
-            bot.sendChat('format is !roll or !roll d20');
-            return;
-        }
-            
-        bot.sendChat("you rolled a " + num);
-        break;
-    
-    case "throw":
-        if(data.args[0] != "rock" && data.args[0]!="paper"&&data.args[0]!="scissors") {
-            bot.sendChat('throw rock, paper or scissors.');
-            return;
-        }
-        var botThrow = Math.floor(Math.random() * 3), // 0-rock,1-paper,2-scissors
-            w = data.args[0],
-            v = ['rock','paper','scissors'],
-            usrThrow = (w=="rock")?0:(w=="paper")?1:w=="scissors"?2:null;
-        if(botThrow==usrThrow){
-            bot.sendChat('both threw '+w+' - tie!');
-        }
-        else if((botThrow==0 && usrThrow == 2) || botThrow -1 == usrThrow)
-            bot.sendChat('bot won with ' + v[botThrow]);
-        else if(usrThrow -1 == botThrow || (botThrow == 2 && usrThrow == 0))
-            bot.sendChat('bot threw ' + v[botThrow] + '. you win!');
-        break;
-            
-
-    case "balance": 
-        dogecoin.exec('getBalance',data.message.fromID,
-            function(err,balance){
-                bot.sendChat(data.message.from+"'s balance is " + balance);
-            }
-        )
-        //bot.sendChat();
-        break;
-    case "tip":
-        tipDoge(data.message.fromID,data.message.from,data.args[0],data.args[1]);
-        break;
-	case "deposit":
-        depositDoge(data.message.fromID, data.message.from);
-        break;
-    case "withdraw":
-        withdrawDoge(data.message.fromID,data.args[0],data.args[1]);       
-        break;
-    case "":break;
-    case "commands": bot.sendChat('commands are: !deposit, !withdraw, !tip, !balance');break;
-        
-    //default: bot.sendChat(data.message.from+' ' + insult[Math.floor(Math.random()*insult.length)]);
-
+        case "clap": 
+            bot.sendChat(":clap: :clap: :clap: :clap: BRAVO!");
+            break;
+        case "moo": 
+            bot.sendChat(":cow: m0000000 :cow2:");
+            break;
+        case "roll":
+            var rollResult = misc.roll(data.args[0]);
+            bot.sendChat(rollResult);
+            break;
+        case "throw": //paper rock or scissors
+            var rpsResult = misc.rockPaperScissors(data.args[0]);
+            bot.sendChat(rpsResult);
+            break;
+        case "balance": 
+            dogeBalance(data.message.fromID, data.message.from);
+            break;
+        case "tip":
+            tipDoge(data.message.fromID, data.message.from, data.args[0], data.args[1]);
+            break;
+        case "deposit":
+            depositDoge(data.message.fromID, data.message.from);
+            break;
+        case "withdraw":
+            withdrawDoge(data.message.fromID, data.args[0], data.args[1]);       
+            break;
+        case "commands": 
+            bot.sendChat('commands are: !deposit, !withdraw, !tip, !balance');
+            break;
+        case "":break;  
+        default: 
+            bot.sendChat(data.message.from+' ' +insult[Math.floor(Math.random()*insult.length)]);
     }
-    
 });
 
-// TIPPING FUNCTIONS...
-
-
+/* Function refreshRedis
+ * if the bot has joined a new room, we need to refresh the userid <--> username database.
+ * due to a flaw in the plugapi, this also needs to be called if someone changes their username while in the room. 
+ **/
 function refreshRedis(){
     var users = bot.getUsers();
     rClient.select(0);
     for(var i=0;i<users.length;i++){
-        /*
-
-        rClient.get(users[i].username, function(err,reply){
-            console.log('getuser err,reply: ' + err + ", " + reply);
-        });
-
-        */
-
         rClient.set(users[i].username,users[i].id);
-
     }
     rClient.select(1);
     for(var i=0;i<users.length;i++){
-
         rClient.set(users[i].id,users[i].username);
-
     }
 }
 
+/* Dogecoin tipping functions. */
+
+function dogeBalance(fromID,from){
+    dogecoin.exec('getBalance',fromID,
+        function(err,balance){
+            bot.sendChat(from+"'s balance is " + balance);
+        }
+    )
+}
+
 function tipDoge(userID, username, recipient, amt, refresh){
-
-    console.log('amt: ' + amt + " " + typeof amt);        
-
-    // !tip DJ xx to tip current DJ is buggy at the moment.
-
-    // var tipType = 0; //0:tip user, 1:tip dj, 2:host 
-    /* if(recipient.toLowerCase() == "dj"){ recipient = currentDJ.username; tipType=1;}*/
-
-    // CHECK FORMAT...
-
     if(recipient==''|| recipient == null || amt=='' || amt==null){
         bot.sendChat("format is: !tip [username] [amount]");
         return;
     }
-
+    
     var tipamt = Math.floor(parseFloat(amt));
-
-    // RETRIEVE sender's USERID, USING USERNAME
-
-    rClient.select(0); // select username -> userid database 
-    rClient.get(recipient,function(err,recipientID){
-
-        if(err || recipientID == null){ // username /id pair does not exist
+    rClient.select(0); // selects username -> userid database 
+    rClient.get(recipient, function(err,recipientID){ // check for a userID matching username
+        
+        if(err || recipientID == null){ // if username /id pair does not exist, refresh redis and call this function again.
             if(typeof refresh === 'undefined'){
                 refreshRedis();
-
-                // refresh is true, so if it errors next time, they arent a real user.
+                // next time refresh is true, so if it errors next time, we know they arent a real user.
                 tipDoge(userID,username,recipient,amt,true); 
                 return;
             } 
             bot.sendChat(recipient + ' is not in the room.');
             return;
-
-        }else{
-            if (isNaN(tipamt)){ bot.sendChat("tip amount is not valid"); return; }
-            if( tipamt <= 0) { bot.sendChat("NO YOU CANT DO THAT");return;}
+        }else{ // userID is found. check for invalid values before tipping.
+            if(isNaN(tipamt)){ bot.sendChat("tip amount is not valid"); return; }
+            if(tipamt <= 0) { bot.sendChat("NO YOU CANT DO THAT");return;}
             dogecoin.exec('getbalance', userID, function(err, bal){
                 if(bal<tipamt){
                     bot.sendChat('insufficient Balance, ' + username);
                     return;
                 }
-
                 dogecoin.move(userID,recipientID,tipamt,function(err,moveinfo){
                     bot.sendChat(username + ' tipped '+ recipient + " " + tipamt + " doge!");
                 });
@@ -237,13 +215,14 @@ function tipDoge(userID, username, recipient, amt, refresh){
 }
 
 function withdrawDoge(userID, destWallet){
-    //var wallet;
+    
+    //TODO: replace with chaining syntax to do away with nested function calls. 
 
-    //FIXME
-
+    // flow: check balance, validate destination wallet, send balance.
+    
     dogecoin.exec('getbalance',userID,
         function(err,balance){
-            if(err){bot.sendChat('error.');return;}
+            if(err){return;}
 
             this.validateAddress(destWallet,
                 function(err, results){
@@ -252,8 +231,7 @@ function withdrawDoge(userID, destWallet){
                         bot.sendChat('bad address. !withdraw [address]');
                         return;
                     }
-                    // console.log('results:');console.log(results);
-                    
+
                     var send_amt = balance - tx_fee;
                     if(send_amt <= 0){
                         bot.sendChat("too small to send!");
@@ -269,7 +247,6 @@ function withdrawDoge(userID, destWallet){
                             console.log('err:'); console.log(err);
                         }
                     );
-
                 }
             );
         }
@@ -279,14 +256,9 @@ function withdrawDoge(userID, destWallet){
 function depositDoge(userID,username){
     var wallet;
     dogecoin.exec('getaddressesbyaccount',userID, function(err,info){
-        if(info.length==0){
-            //
+        if(info.length==0){ // if no addresses associated with this ID, create one
             this.getNewAddress(userID, function(err,address){
-                if(err!=null){
-                    //connection.end();
-                    return;
-                }
-                console.log(address);
+                if(err!=null){ return }
                 bot.sendChat(username+'\'s deposit wallet: ' + address);
             })
         }
@@ -295,10 +267,5 @@ function depositDoge(userID,username){
         }
     });
 }
-
-/*
-TODO: tip song for creator
-TODO: tip dj
-*/
 
 }).call(this)
